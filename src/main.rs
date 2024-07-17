801576ba2cbb32a853e19a7697a292397ed5def9
JuanMarceloT <jmarcelot2013@gmail.com>
2024-07-17 16:57:08 -0300

    removing unused code

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,32 @@
-
-use std::error::Error;
-use std::fs::File;
-use std::path::Path;
 use std::time::{Duration, Instant};
 
-#[derive(Debug, Clone)]
-struct Player {
-    id: i32,
-    name: String,
-    player_positions: Vec<String>,
-}
-
-fn insert(num: Player, module: usize, hash_table: &mut[Vec<Player>]){
-    let index = (num.id as usize) % module;
+fn insert(num: i32, module: usize, hash_table: &mut[Vec<i32>]){
+    let index = (num as usize) % module;
     hash_table[index].push(num);
 }
 
-fn search(id: i32, module: usize, hash_table: &[Vec<Player>]) ->  Option<Player> {
+fn search(id: i32, module: usize, hash_table: &[Vec<i32>]) ->  Option<i32> {
     let index = (id as usize) % module;
     
-    for player in &hash_table[index] {
-        if player.id == id {
-            return Some(player.clone());
-        }
+    for nums in &hash_table[index] {
+        return Some(*nums);
     }
     None
 }
 
-
-fn read_csv<P, F>(filename: P, mut func: F) -> Result<(), Box<dyn Error>>
-where
-    P: AsRef<Path>,
-    F: FnMut(Player),
-{
-    let file = File::open(filename)?;
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.records() {
-        let record = result?;
-        if record.len() == 3 {
-            let player = Player {
-                id: record[0].parse::<i32>()?,
-                name: record[1].to_string(),
-                player_positions: record[2].split(',').map(|s| s.to_string()).collect(),
-            };
-            
-            func(player);
-        }
-    }
-    Ok(())
-}
-
-fn measure_time<F>(func: F) -> ()
+fn measure_time<F>(func: F) -> Duration
 where
     F: FnOnce(),
 {
     let start = Instant::now();
     func();
     let duration = start.elapsed();
-    println!("Tempo gasto: {:?}", duration);
+    //println!("Tempo gasto: {:?}", duration);
+    duration
 }
-
 fn main() {
 
     const MODULE : usize = 25; 
-    const ARRAY_REPEAT_VALUE: Vec<Player> = Vec::new();
-    let mut hash_table : [Vec<Player>; MODULE] = [ARRAY_REPEAT_VALUE; MODULE];
-
-
-
-    let _result = read_csv("players.csv", |player| {
-        insert(player, MODULE, &mut hash_table);
-    });
-
-    measure_time(|| {
-        println!("{:?}", search(158023, MODULE, &hash_table).unwrap().name);
-    });
-
-
+    const ARRAY_REPEAT_VALUE: Vec<i32> = Vec::new();
+    let mut hash_table : [Vec<i32>; MODULE] = [ARRAY_REPEAT_VALUE; MODULE];
 }
